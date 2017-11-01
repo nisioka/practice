60d67dd916d9c609f884c4dc9e0816b919e20862
unknown <daisuke.nishioka@avantcorp.com>
2017-11-01 14:36:05 +0900

    test PR.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DemoApplication
+ * @since 1.0.0
+ */
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * main.
+	 * @param args arguments
+	 */
 	public static void main(String[] args) {
+		System.out.println("hoge fuga piyo");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
